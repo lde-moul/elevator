a426a87bd1fa6bcf3ef5dc802888d14dc5344b8f
Louis-Antoine DE MOULINS DE ROCHEFORT <lde-moul@student.42.fr>
2022-12-06 09:41:32 +0100

    Register elevators with different properties

--- src/shared/Util.tsx
+++ src/shared/Util.tsx
@@ -5,3 +5,16 @@ export const getFilledArray = (length: number, value: any): Array<any> =>
 
 export const getFloorName = (floor: number): string =>
   (floor !== 0) ? floor.toString() : 'RDC';
+
+export const elevatorProperties = [
+  {
+    bottom: 0,
+    top: 9,
+    condition: () => true
+  },
+  {
+    bottom: -5,
+    top: 9,
+    condition: (floor) => floor < 0 || floor > 8 || floor % 2 === 0
+  },
+];
